468c1be5a1ca01ea802eced25f8e997ac7cbb55c
cexposit <cexposit@ull.edu.es>
2019-09-06 09:33:10 +0100

    kaizten-algorithm docker

--- script-commit-github.sh
+++ script-commit-github.sh
@@ -7,5 +7,5 @@ git add *
 git config --global user.email "cexposit@ull.edu.es"
 git config --global user.name "cexposit"
 git commit -m "${commitMessage}"
-git remote add origin htpps://KaizTen/kaizten-configuration-repository
+git remote add origin htpps://github.com/KaizTen/kaizten-configuration-repository
 git push -u origin master
